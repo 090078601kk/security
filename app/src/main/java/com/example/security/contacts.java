a799e4806b305ff675d810d004a857d4bccc9f01
Kashaf Khan <kashafkhan1499@gmail.com>
2020-06-24 18:18:21 +0500

    VOLLEY DEPENDENCY ADDED
    Signed-off-by: Kashaf Khan <kashafkhan1499@gmail.com>

--- app/src/main/java/com/example/security/contacts.java
+++ app/src/main/java/com/example/security/contacts.java
@@ -19,11 +19,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
